2739e69c45afa70c704dfd9e42ef39e675b60566
vasanth064 <vasanthvdev@gmail.com>
2024-02-03 13:40:20 +0530

    added form and polls delete

--- src/pages/ViewPolls.jsx
+++ src/pages/ViewPolls.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getPollStructure } from '../api/polls';
+
 const ViewPolls = () => {
-  return <div>ViewPolls</div>;
+  const [form, setForm] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+    (async () => {
+      const data = await getPollStructure(params.id);
+      setForm(data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h1>View Polls</h1>
+      {JSON.stringify(form)}
+    </div>
+  );
 };
 
 export default ViewPolls;
