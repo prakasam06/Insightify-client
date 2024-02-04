5191dbddad3efde4719dc51263ea443ed629e833
Vasanth V V <vasanthvdev@gmail.com>
2024-02-04 16:36:55 +0530

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 4173,
+  preview: {
+    host: true,
+    port: 8000,
     strictPort: true,
   },
+
   plugins: [react()],
 });
